cc4e4047036b1dbd8dc89605fe8c480068e60f08
Adel Ghaedi <adelghaedii12@gmail.com>
2026-06-07 11:20:17 +0330

    [UP] translation and rtl static file

--- static/js/article_like_ajax.js
+++ static/js/article_like_ajax.js
@@ -1,7 +1,8 @@
 function like(slug) {
     const like_element = document.getElementById("like_tag")
     const count_like_element = document.getElementById("count_like_tag")
-    $.get(`/article/like/${slug}`).then(response => {
+    const currentLang = window.location.pathname.split('/')[1] // 'en' یا 'fa'
+    $.get(`${currentLang}/article/like/${slug}`).then(response => {
         if (response["liked"]) {
             like_element.className = "fa fa-heart-o"
             count_like_element.innerText = String(Number(count_like_element.innerText) - 1)
