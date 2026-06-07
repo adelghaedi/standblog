7389b532bc25cf8357fbd0178abfef2415e44234
Adel Ghaedi <adelghaedii12@gmail.com>
2026-06-07 08:29:22 +0330

    [UP] translation and rtl

--- account/forms.py
+++ account/forms.py
@@ -2,21 +2,22 @@ from django import forms
 from django.contrib.auth.models import User
 from django.core.validators import ValidationError
 from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(
         max_length=20,
-        widget=forms.TextInput(attrs={'class': 'input100', "placeholder": "Username"})
+        widget=forms.TextInput(attrs={'class': 'input100', "placeholder": _("Username")})
     )
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input100', "placeholder": "Password"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input100', "placeholder": _("Password")}))
 
     def clean(self):
         username = self.cleaned_data.get('username')
         password = self.cleaned_data.get('password')
 
         if authenticate(username=username, password=password) is None:
-            raise ValidationError("Incorrect username or password.")
+            raise ValidationError(_("Incorrect username or password."))
 
 
 class RegisterForm(forms.ModelForm):
@@ -24,11 +25,11 @@ class RegisterForm(forms.ModelForm):
         model = User
         fields = ("first_name", "last_name", "email", "username", "password")
         widgets = {
-            "first_name": forms.TextInput(attrs={"class": "input100", "placeholder": "FirstName"}),
-            "last_name": forms.TextInput(attrs={"class": "input100", "placeholder": "LastName"}),
-            "email": forms.EmailInput(attrs={"class": "input100", "placeholder": "Email"}),
-            "username": forms.TextInput(attrs={"class": "input100", "placeholder": "Username"}),
-            "password": forms.PasswordInput(attrs={"class": "input100", "placeholder": "Password"}),
+            "first_name": forms.TextInput(attrs={"class": "input100", "placeholder": _("FirstName")}),
+            "last_name": forms.TextInput(attrs={"class": "input100", "placeholder": _("LastName")}),
+            "email": forms.EmailInput(attrs={"class": "input100", "placeholder": _("Email")}),
+            "username": forms.TextInput(attrs={"class": "input100", "placeholder": _("Username")}),
+            "password": forms.PasswordInput(attrs={"class": "input100", "placeholder": _("Password")}),
         }
 
 
@@ -37,7 +38,7 @@ class UserEditForm(forms.ModelForm):
         model = User
         fields = ("first_name", "last_name", "email")
         widgets = {
-            "first_name": forms.TextInput(attrs={"class": "input100", "placeholder": "FirstName"}),
-            "last_name": forms.TextInput(attrs={"class": "input100", "placeholder": "LastName"}),
-            "email": forms.EmailInput(attrs={"class": "input100", "placeholder": "Email"}),
+            "first_name": forms.TextInput(attrs={"class": "input100", "placeholder": _("FirstName")}),
+            "last_name": forms.TextInput(attrs={"class": "input100", "placeholder": _("LastName")}),
+            "email": forms.EmailInput(attrs={"class": "input100", "placeholder": _("Email")}),
         }
